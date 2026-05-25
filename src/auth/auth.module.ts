d4565257f3fa977376a3533a74dc914aaf279953
Agus <aguspaidw4@gmail.com>
2026-05-25 13:13:25 -0300

    JWT terminado

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+
 import { JwtModule } from '@nestjs/jwt';
+
+import { PassportModule } from '@nestjs/passport';
+
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+
 import { UsersModule } from '../users/users.module';
 
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+
+    PassportModule,
 
     JwtModule.registerAsync({
       imports: [ConfigModule],
@@ -26,6 +35,10 @@ import { UsersModule } from '../users/users.module';
   ],
 
   controllers: [AuthController],
-  providers: [AuthService],
+
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,17 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
+
 import * as bcrypt from 'bcrypt';
 
 import { UsersService } from './users.service';
+
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -25,5 +31,11 @@ export class UsersController {
       name: body.name,
       password: hashedPassword,
     });
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@Request() req: any) {
+    return req.user;
   }
 }

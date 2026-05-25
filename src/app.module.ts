bc06102bf47ccbc73407fb1909e9feab0c2c9902
Agus Pai <aguspaidw4@gmail.com>
2026-05-25 14:57:37 +0000

    Estructura completa

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,16 @@ import { AppController } from './app.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { StockModule } from './stock/stock.module';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
   imports: [
@@ -28,6 +38,26 @@ import { AppService } from './app.service';
         synchronize: true,
       }),
     }),
+
+    AuthModule,
+
+    UsersModule,
+
+    ProductsModule,
+
+    CategoriesModule,
+
+    SuppliersModule,
+
+    CustomersModule,
+
+    OrdersModule,
+
+    PaymentsModule,
+
+    StockModule,
+
+    PurchasesModule,
   ],
 })
 export class AppModule {}
